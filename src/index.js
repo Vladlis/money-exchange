670128190e43681862f30c1dd261b9b043aa37b4
Vladislav Lisetskii <vladislav_lisetskii@epam.com>
2019-03-04 18:08:37 +0300

    feat: implement function makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency <= 0) {
+        return {};
+    }
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+
+    var result = new Object();
+    var h = Math.floor(currency / 50);
+    if (h > 0) {
+        result.H = h;
+    }
+    var q = Math.floor(currency % 50 / 25);
+    if (q > 0) {
+        result.Q = q;
+    }
+    var d = Math.floor(currency % 25 / 10);
+    if (d > 0) {
+        result.D = d;
+    }
+    var n = Math.floor(currency % 25 % 10 / 5);
+    if (n > 0) {
+        result.N = n;
+    }
+    var p = currency % 5;
+    if (p > 0) {
+        result.P = p;
+    }
+    return result;
 }
